2dd9bb6e0f9d8dc8252805e7f59daa9261311f00
Erika Balode <erika.balode@gmail.com>
2014-04-17 01:22:56 +0100

    to use in a theme template file

---
+++ theme-options/template-file.php
@@ -0,0 +1,34 @@
+         <p>PHONE: <?php
+            $options = get_option( 'cb_options' );
+            echo $options['contacts_phone'];
+         ?></p>
+         
+         <p>EMAIL: <?php
+            $options = get_option( 'cb_options' );
+            echo $options['contacts_email'];
+         ?></p>     
+         
+         <p>ADDRESS: <?php
+            $options = get_option( 'cb_options' );
+            echo $options['contacts_address'];
+         ?></p>
+         
+          <p>TWITTER: <?php
+            $options = get_option( 'cb_options' );
+            echo $options['social_twitter'];
+         ?></p>
+         
+         <p>FACEBOOK: <?php
+            $options = get_option( 'cb_options' );
+            echo $options['social_facebook'];
+         ?></p>
+         
+         <p>G+: <?php
+            $options = get_option( 'cb_options' );
+            echo $options['social_google'];
+         ?></p>
+         
+         <p>Linkedin: <?php
+            $options = get_option( 'cb_options' );
+            echo $options['social_linkedin'];
+         ?></p>

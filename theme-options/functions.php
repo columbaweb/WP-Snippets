5f626b1b2882dd2331b5b47a558ed43e8897fe6b
Erika Balode <erika.balode@gmail.com>
2014-04-17 01:33:11 +0100

    Update functions.php

--- theme-options/functions.php
+++ theme-options/functions.php
@@ -1,1 +1,8 @@
 require_once ( get_stylesheet_directory() . '/theme-options.php' );
+
+# ADD ADMIN CSS
+function add_admin_style() {
+    echo '<link rel="stylesheet" 
+        href="'.get_bloginfo('template_url').'/admin-style.css">'; //adjust your path
+}
+add_action('admin_head', 'add_admin_style');
